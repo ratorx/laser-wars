14ae73bd52945943e92541b5a250ed6ada667b71
CS50Creator1 <68sharmaani@gmail.com>
2016-12-02 17:59:57 +0000

    Laserbullet

---
+++ Assets/LaserBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBullet : MonoBehaviour {
+
+    public GameObject prefab;
+    // Use this for initialization
+    void Start () {
+        Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
