09366a56dc54220d40a45fbfeeb6b40225753d0d
PhoebeMay <phoebenichols@gmail.com>
2016-12-02 14:57:27 +0000

    Circle black hole added

---
+++ Assets/circleblackhole.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circleblackhole : MonoBehaviour 
+{
+
+	private float RotateSpeed = 0.5f;
+	private float Radius = 2f;
+
+	private Vector2 _centre;
+	private float _angle;
+
+	private void Start()
+	{
+		_centre = transform.position;
+	}
+
+	private void Update()
+	{
+
+		_angle += RotateSpeed * Time.deltaTime;
+
+		var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+		transform.position = _centre + offset;
+	}
+
+
+
+}
