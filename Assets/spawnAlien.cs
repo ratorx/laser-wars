4f56ff78f8bbdadeab37c15cf0346dc87512de4c
PhoebeMay <phoebenichols@gmail.com>
2016-12-02 16:31:52 +0000

    Good alien image

---
+++ Assets/spawnAlien.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class spawnAlien : MonoBehaviour
+{
+	public GameObject prefab;
+
+	void Start()
+	{
+		for (int i = 0; i < 10; i++)
+			Instantiate(prefab, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+	}
+}
