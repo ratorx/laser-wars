4c5ff618cac20e4d8c098b6df62fc82111bafdad
ratorx <reetobratachatterjee@gmail.com>
2016-12-02 14:53:58 +0000

    Added vertical movement and horizontal rotation

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        var move = new Vector3(0, Input.GetAxis("Vertical"), 0);
+
+        // transform.Rotate(10 * Vector3.forward * Time.deltaTime);
+        transform.position += move * speed * Time.deltaTime;
+
+        //Vector3 v3 = new Vector3(0, Input.GetAxis("Horizontal"), 0);
+        transform.Rotate(0.0f , 0.0f, -Input.GetAxis("Horizontal") * speed);
+        // transform.Rotate(v3 * speed * Time.deltaTime);
+    }
+}
