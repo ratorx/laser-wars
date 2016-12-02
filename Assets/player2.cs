d2bb958c0d7618aedc665e3defc08d57d8631188
Ben Tatman <ben@tatmans.co.uk>
2016-12-02 19:29:43 +0000

    Fix

--- Assets/player2.cs
+++ Assets/player2.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class player2 : MonoBehaviour
 {
+public float xMax = 10;
+  public float yMax = 5;
   public float speed = 2f;
 	public Sprite stop = Resources.Load("sadbadrocket", typeof(Sprite)) as Sprite;
 	public Sprite start = Resources.Load("badrocket", typeof(Sprite)) as Sprite;
@@ -27,6 +29,16 @@ public class player2 : MonoBehaviour
 		}
         //Vector3 v3 = new Vector3(0, Input.GetAxis("Horizontal"), 0);
         transform.Rotate(0.0f , 0.0f, -(((Input.GetKey(KeyCode.L)) ? 1:0) - ((Input.GetKey(KeyCode.J))?1:0)) * speed);
+	  if (transform.position.x > xMax) {
+  transform.position = new Vector3(-xMax, transform.position.y, transform.position.z);
+} else if (transform.position.x < -xMax) {
+  transform.position = new Vector3(xMax, transform.position.y, transform.position.z);
+}
+if (transform.position.y > yMax) {
+  transform.position = new Vector3(transform.position.x, -yMax, transform.position.z);
+} else if (transform.position.y < -yMax) {
+  transform.position = new Vector3(transform.position.x, yMax, transform.position.z);
+}
         // transform.Rotate(v3 * speed * Time.deltaTime);
     } // Dun goofed.
 }
