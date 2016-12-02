477eb34a49004f220eb8915e37b45a22e14cbbe3
PhoebeMay <phoebenichols@gmail.com>
2016-12-02 14:01:10 +0000

    Added black hole

---
+++ Assets/rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class rotate : MonoBehaviour 
+{
+	void Update() 
+	{
+		Vector3 all = new Vector3 (1, -3, 2);
+		// Rotate the object around its local X axis at 1 degree per second
+		transform.Rotate(50 * all * Time.deltaTime );
+		// Random r = new Random ();
+		float bound = 0.2f;
+		Vector3 movement = new Vector3 (Random.Range(-bound, bound), Random.Range(-bound, bound), Random.Range(-bound, bound));
+		transform.position += movement;
+
+		// ...also rotate around the World's Y axis
+		//transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+	}
+}
